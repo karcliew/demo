11d9c146839d94c0bfdea40d75212780c279d325
karcliew <karc.liew@gmail.com>
2015-06-26 07:36:41 -0500

    Mac changes

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World, me again");
+        System.out.println("Hello World, me again, mac branch");
     }
     
 }
